acc836bc91c8d5a134ca90e823a539048049d62c
Cristian Burgos <cristian.burgos.utepsa@gmail.com>
2017-09-03 01:46:03 -0400

    RC-02: Images added in gallery

--- app/gallery/gallery.controller.js
+++ app/gallery/gallery.controller.js
@@ -1,3 +1,29 @@
 /**
  * Created by BURGOS on 27/08/2017.
  */
+;(function () {
+    angular.module('aranjuez').controller('galleryController', galleryController);
+
+    galleryController.$inject = ['$scope'];
+
+    function galleryController($scope) {
+        $scope.dataImages = [
+            {name: "images/galeria/imagen1.jpg"},
+            {name: "images/galeria/imagen2.jpg"},
+            {name: "images/galeria/imagen3.jpg"},
+            {name: "images/galeria/imagen4.jpg"},
+            {name: "images/galeria/imagen5.jpg"},
+            {name: "images/galeria/imagen6.jpg"},
+            {name: "images/galeria/imagen7.jpg"},
+            {name: "images/galeria/imagen8.jpg"},
+            {name: "images/galeria/imagen9.jpg"},
+            {name: "images/galeria/imagen10.jpg"}
+        ];
+
+        $scope.activateMaterialboxed = function () {
+            console.log('a;dsfsd');
+            $('.materialboxed').materialbox();
+        };
+    }
+
+})();
