efa1f9251512ad311a0ea163948ae784a60fb7f0
cristian <cristian.burgos.utepsa@gmail.com>
2017-09-15 14:15:33 -0400

    RC-03: Animated css plugin added to project

--- app/contactUs/contactUs.controller.js
+++ app/contactUs/contactUs.controller.js
@@ -2,10 +2,21 @@
  * Created by BURGOS on 26/08/2017.
  */
 ;(function () {
-    angular.module('aranjuez').controller('informationOfClubController', informationOfClubController);
-    informationOfClubController.$inject = ['$scope'];
+    angular.module('aranjuez').controller('contactUsController', contactUsController);
+    contactUsController.$inject = ['$scope'];
 
-    function informationOfClubController($scope) {
-        $('ul.tabs').tabs();
+    function contactUsController($scope) {
+        $scope.modelContactUs = [
+            {
+                name: 'Jose Manuel Mendoza',
+                telephone: '70971826',
+                email: 'josemanueljacku16@gmail.com'
+            },
+            {
+                name: 'Alberto Zunino Escribano',
+                telephone: '73697222',
+                email: 'aranjuezrugbyclub@gmail.com '
+            }
+        ];
     }
 })();
--- app/informationOfClub/informationOfClub.controller.js
+++ app/informationOfClub/informationOfClub.controller.js
@@ -2,21 +2,92 @@
  * Created by BURGOS on 03/09/2017.
  */
 ;(function () {
-    angular.module('aranjuez').controller('contactUsController', contactUsController);
-    contactUsController.$inject = ['$scope'];
+    angular.module('aranjuez').controller('informationOfClubController', informationOfClubController);
+    informationOfClubController.$inject = ['$scope'];
 
-    function contactUsController($scope) {
-        $scope.modelContactUs = [
+    function informationOfClubController($scope) {
+        $scope.showingMoreHistoryOfClub = false;
+        $scope.tabs = 'history';
+
+        $scope.showMoreHistoryOfClub = function (state) {
+            $scope.showingMoreHistoryOfClub = state;
+        };
+
+        $scope.ChangeTab = function (tab) {
+            $scope.tabs = tab
+        }
+
+        $scope.dataPlayers = [
             {
-                name: 'Jose Manuel Mendoza',
-                telephone: '70971826',
-                email: 'josemanueljacku16@gmail.com'
+                id: 1,
+                name: 'Juan Perez',
+                edad: 20,
+                Poscision: 'lorem',
+                dorsa: 10,
+                inicioEnElClub: '21/12/2016',
+                tipoDeEquipo: 'Division Masculina'
             },
             {
-                name: 'Alberto Zunino Escribano',
-                telephone: '73697222',
-                email: 'aranjuezrugbyclub@gmail.com '
+                id: 2,
+                name: 'Maria Lozada',
+                edad: 20,
+                Poscision: 'lorem lorem',
+                dorsa: 11,
+                inicioEnElClub: '21/09/2016',
+                tipoDeEquipo: 'Division Femenina'
+            },
+            {
+                id: 3,
+                name: 'Julio Perez',
+                edad: 20,
+                Poscision: 'lorem lorem',
+                dorsa: 11,
+                inicioEnElClub: '21/09/2016',
+                tipoDeEquipo: 'Division Masculina'
+            },
+            {
+                id: 4,
+                name: 'Maria Mendez',
+                edad: 20,
+                Poscision: 'lorem lorem',
+                dorsa: 11,
+                inicioEnElClub: '21/09/2016',
+                tipoDeEquipo: 'Division Femenina'
+            },
+            {
+                id: 5,
+                name: 'Juan Banzer',
+                edad: 20,
+                Poscision: 'lorem lorem',
+                dorsa: 11,
+                inicioEnElClub: '21/09/2016',
+                tipoDeEquipo: 'Division Masculina'
+            },
+            {
+                id: 6,
+                name: 'Maria Mercado',
+                edad: 20,
+                Poscision: 'lorem lorem',
+                dorsa: 11,
+                inicioEnElClub: '21/09/2016',
+                tipoDeEquipo: 'Division Femenina'
             }
         ];
+
+        $scope.teamTypes = [
+            {
+                id: 1,
+                name: 'Division Masculina'
+            },
+            {
+                id : 2,
+                name: 'Division Femenina'
+            }
+        ]
+        
+        $scope.showModalInfoPlayer = function (dataOfPlayers) {
+            //alert('llega')
+            $('#modal').openModal();
+        }
     }
 })();
